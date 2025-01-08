5cf08465d09216b9c4007ef8d0b8160cdc181369
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2025-01-08 14:25:38 +0500

    Working on CH-03

--- chapters/03_control-flows.js
+++ chapters/03_control-flows.js
@@ -3,10 +3,43 @@
 ************************************* */
 
 // 01 Logic and Control Flow
+const age = 18;
+
+if (age > 18) {
+    console.log('You may enter, welcome');
+} else if (age === 18) {
+    console.log('You just turned 18, you may enter, welcome');
+} else {
+    console.log('You are not allowed to enter');
+}
+
 
 // 02 Truty/Falsy Values
+/* 
+    The understanding of Truty and Falsy values is important when working with control flow statements.
+    - Truty values are considered true when evaluated in a boolean context, while Falsy values are considered false.    
+
+    JavaScript only has 6 Falsy Values:
+    1. undefined // variable that has not been assigned a value
+    2. null // null is an empty value
+    3. 0 // number zero
+    4. '' // empty string
+    5. NaN // Not a Number
+    6. false // boolean false
+
+    Everything else is considered Truty, for example:
+    1. '0' // string with a zero
+    2. ' ' // string with a space
+    3. [] // empty array
+    4. {} // empty object
+    5. function() {} // empty function
+    6. true // boolean true
+
+*/
+
 
 // 03 Logical Operators Part 2
+
 
 // 04 And Operator (&&)
 
