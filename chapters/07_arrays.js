1a84b0c9bf3e5939e8518241c997c7d6cb40a1eb
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2025-01-10 02:55:39 +0500

    Working on CH-07 Arrays

--- chapters/07_arrays.js
+++ chapters/07_arrays.js
@@ -3,6 +3,41 @@
 ************************************* */
 
 // 📌 01 Arrays
+const friends = ['Michael', 'Steven', 'Peter', 'John'];
+console.log(friends[0]);
+
+// To replace an element in an array
+friends[2] = 'Jay';
+console.log(friends);
+
+// To add a new element in an array
+friends[4] = 'Bob';
+console.log(friends);
+
+// To find the length of an array
+console.log(friends.length);
+
+// An array can storre different data types
+const arr = [
+    'Apple',
+    {
+        name: 'John', 
+        age: 25,
+        marks: 75
+    },
+    true,
+    function() {
+        console.log('Hello World!')
+    }
+]
+
+// To loop through an array
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+for(let i = 0; i < months.length; i++) {
+    console.log(months[i]);
+}
+
 
 // 📌 02 Array Methods
 
